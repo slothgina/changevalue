ffa31df5874ace6679a04c5a6205683616239c43
slothgina <gina.slothstudio@zohomail.com>
2026-05-04 16:46:57 -0400

    The code comment describes the desired functionality.
    Run the application in the Visual Studio Code debugger.
    
    Check the output produced.
    
    Use the C# debugger tools to isolate the issue.
    
    Consider how you might update the code to match the desired functionality.

--- obj/Debug/net10.0/changevalue.AssemblyInfo.cs
+++ obj/Debug/net10.0/changevalue.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("changevalue")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+346d760ef7e0daf518c70d064555217cbecbbc7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("changevalue")]
 [assembly: System.Reflection.AssemblyTitleAttribute("changevalue")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
